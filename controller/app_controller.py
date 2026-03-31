3666b96d42f22a2b838a28fd7cd6d1a878ccaf8c
Gustavo Miranda <137585984+gustavomiranda2302@users.noreply.github.com>
2026-03-31 19:29:05 -0300

    feat: adicionado o return na revisar_deck e o criar_card

--- controller/app_controller.py
+++ controller/app_controller.py
@@ -8,4 +8,24 @@ class AppController:
         self.cardbo = cardbo
         self.deckservice = deckservice
         
+    
+    def revisar_card(self, card_id, nota, deck_id):
+        deck_escolhido =self.deckservice.get_id_deck(deck_id)
+        for i in deck_escolhido.cards:
+            if i.id == card_id:
+                card_escolhido = i
+                self.cardbo.revisar_card(card_escolhido, nota)
+                
+        return self.deckservice.atualizar_deck(deck_escolhido)
+    
+    def criar_card(self,deck_id,card):
+        deck_escolhido =self.deckservice.get_id_deck(deck_id)
+        deck_escolhido.cards.append(card)
+        return self.deckservice.atualizar_deck(deck_escolhido)
         
+               
+    
+
+
+    
+    
